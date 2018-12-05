c2d4378946ff4b4c573722ce79bd1b1a020fef31
Tory Dannenberg <rfdberg@gmail.com>
2018-12-05 08:14:53 -0700

    Remove logo and change double quotes to single quotes for consistency

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,8 +5,8 @@ import { Route, NavLink } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <div className='links-container'>
             <NavLink to='/' className='nav'> Home </NavLink>
             <NavLink to='/about' className='nav'> About Me </NavLink>
